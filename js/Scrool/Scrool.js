f21c69b7095ce835a43713d9381432d66f0b85ec
Jesus cueva <jcueva12380@gmail.com>
2021-06-10 15:47:20 -0500

    subiendo fuetes corregidas

--- js/Scrool/Scrool.js
+++ js/Scrool/Scrool.js
@@ -5,20 +5,23 @@ const d = document,
 const Scroll = ()=>{
     const $home = d.getElementById("home")
     const $header = d.getElementById("_header")
+    const $main = d.getElementById("inicio")
 
     w.addEventListener("scroll", e=>{
         const $scroll = d.documentElement.scrollTop
 
         
         if($scroll > 53){
-            $home.style.transition = ".5s all ease"
+            $home.style.transition = ".4s all ease"
             $home.style.display = "none"
             $header.style.position = "fixed"
             $header.style.boxShadow = "0px 2px 3px 2px #eee"
+            $main.style.marginTop = "180px"
         }else{
             $home.style.display = "block"
             $header.style.position = "relative"
             $header.style.boxShadow = "none"
+            $main.style.marginTop = "0px"
         }
     })
 }
