00e839fbbb9f3bd00ccc2f1b98c325665c4bab3c
Jesus cueva <jcueva12380@gmail.com>
2021-06-14 12:03:52 -0500

    subiendo animaciones corregidas scrool

--- js/Scrool/Scrool.js
+++ js/Scrool/Scrool.js
@@ -14,19 +14,24 @@ const Scroll = ()=>{
     w.addEventListener("scroll", e=>{
         const $scroll = d.documentElement.scrollTop
         console.log($scroll)
-
         
-        if($scroll > 53){
+        if($scroll >= 54){
             $home.style.transition = ".4s all ease"
             $home.style.display = "none"
             $header.style.position = "fixed"
             $header.style.boxShadow = "0px 2px 3px 2px #eee"
             $main.style.marginTop = "180px"
+            console.log("scroll",$scroll)
+        }else{
+            $home.style.display = "block"
+            $header.style.position = "relative"
+            $header.style.boxShadow = "none"
+            $main.style.marginTop = "0px"
         }
-        if($scroll > 700){
+        if($scroll >= 700){
             imgHabitus.classList.add("active")
         }
-        if($scroll >820){
+        if($scroll >= 820){
             var time = setTimeout(()=>{
                 imgMobiliario.classList.add("active")
                 imgJc.classList.add("active")
@@ -38,10 +43,6 @@ const Scroll = ()=>{
             imgMobiliario.classList.remove("active")
             imgJc.classList.remove("active")
             imgEjecuta.classList.remove("active")
-            $home.style.display = "block"
-            $header.style.position = "relative"
-            $header.style.boxShadow = "none"
-            $main.style.marginTop = "0px"
             clearTimeout(time)
         }
         
